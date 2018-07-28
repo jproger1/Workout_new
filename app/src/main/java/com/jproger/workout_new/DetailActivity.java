24961c53f8b9b599e7b3fdb997e00ea696f0cabb
Jproger <webscripting@ya.ru>
2018-07-28 08:10:15 +0300

    Add workout

--- app/src/main/java/com/jproger/workout_new/DetailActivity.java
+++ app/src/main/java/com/jproger/workout_new/DetailActivity.java
@@ -9,5 +9,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_fragment);
+        frag.setWorkoutId(0);
     }
 }
